c2789175288a03b61ed959cef84788d491321966
EUPEDROSO02 <guispedroso14@gmail.com>
2025-09-02 10:59:15 -0300

    agora vai

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 console.log("Atividade Semestre")
 
-//nome = String(prompt("Olá! Qual o seu nome? "))
-//alert("Seja bem vindo(a) ao nosso site, "+nome)
-
-claudio = String(prompt("Você gosta do claudio? (S/N)"))
-nome = String(prompt("E qual é o seu nome?"))
-
-alert("Então fodasi "+nome+ " , ngm gosta do cláudio, APENAS O SEBA E O JONATHAS")
+nome = String(prompt("Olá! Qual o seu nome? "))
+alert("Seja bem vindo(a) ao nosso site, "+nome)
