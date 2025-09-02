2e65889d6fc93d0b3b896c1ee6cb772d7ea4cb13
Guilherme Pedroso <125369165+PEDROSO02@users.noreply.github.com>
2025-09-02 00:31:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Atividade Semestre")
+
+//nome = String(prompt("Olá! Qual o seu nome? "))
+//alert("Seja bem vindo(a) ao nosso site, "+nome)
+
+claudio = String(prompt("Você gosta do claudio? (S/N)"))
+nome = String(prompt("E qual é o seu nome?"))
+
+alert("Então fodasi "+nome+ " ngm gosta do cláudio")
